5f8a26d8932ad19c0a75a5af97bbf23febd816eb
Charis <harismaulan6@gmail.com>
2026-02-16 15:06:59 +0700

    fix UI

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -12,7 +12,8 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <link rel="stylesheet" href="{{ url('build/assets/app-BcZymFcn.css') }}">
+        <script src="{{ url('build/assets/app-bxQceY9i.js') }}" defer></script>
     </head>
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
